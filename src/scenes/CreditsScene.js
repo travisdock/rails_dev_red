af084f4416eb608425ae299386410190cf675fea
Travis Dockter <travisdock@gmail.com>
2026-04-21 17:30:25 -0600

    Add some more people

--- src/scenes/CreditsScene.js
+++ src/scenes/CreditsScene.js
@@ -35,6 +35,8 @@ class CreditsScene extends Phaser.Scene {
       { text: 'Irina Nazarova', size: 8 },
       { text: 'Justin Bowen', size: 8 },
       { text: 'Scott Werner', size: 8 },
+      { text: 'Kieran Klaassen', size: 8 },
+      { text: 'Matheus', size: 8 },
       { text: '', size: 8 },
       { text: 'Made for', size: 8, color: '#888888' },
       { text: 'Blastoff Rails 2026', size: 10, color: '#ff6633' },
